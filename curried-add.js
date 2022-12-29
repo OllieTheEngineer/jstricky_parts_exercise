b4da1fa4fb30b2de8e8f96c9d86d80e639258f70
ollie <olivinanzali07@gmail.com>
2022-12-28 20:50:20 -0600

    added curried-add code

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,17 @@
 function curriedAdd(total) {
-
+    if(total === undefined) return 0;
+    return function nextAdder(num){
+        if(num === undefined) return total;
+        total += num;
+        return nextAdder;
+    };
 }
 
 module.exports = { curriedAdd };
+
+/*
+
+1- write a function called curriedAdd
+if you pass a number to this function it will return a new function
+
+*/
