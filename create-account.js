19641014b928c54d921050b0aaf8c0cafce89fad
ollie <olivinanzali07@gmail.com>
2022-12-28 20:33:09 -0600

    adding create-account code

--- create-account.js
+++ create-account.js
@@ -1,5 +1,49 @@
-function createAccount(pin, amount) {
+function createAccount(pin, bal=0) {
+    return {
+       checkBalance(pinInput) {
+        if(pinInput !== pin) return "Invalid PIN."
+        return `$${bal}`
+       },
+       deposit(pinInput, depAmnt){
+        if(pinInput !== pin) return "Invalid PIN."
+        bal+=depAmnt;
+        return `Succesfully deposited $${depAmnt}. Current balance: $${bal}.`
+       },
+       withdraw(pinInput, wthdrawAmnt){
+        if(pinInput !== pin) return "Invalid PIN."
+        if(bal < wthdrawAmnt) {
+            return "Withdrawal amount exceeds account balance. Transaction cancelled."
+        } else {
+            bal -= wthdrawAmnt;
+            return `Succesfully withdrew $${wthdrawAmnt}. Current balance: $${bal}.`
+        }
+       },
+       changePin(pinInput, newPin){
+        if(pinInput !== pin) return "Invalid PIN.";
+        if(pinInput === pin){
+            pin = newPin;
+            return "PIN successfully changed!"
+        }
+       } 
+    }
 
 }
 
 module.exports = { createAccount };
+
+/*
+create a function called createAccount,
+which takes a pin and a deposit amount ex function createAccount(pin, num)
+return value is to be an object {} with 4 methods:
+- checkbalance() 
+if correct pin - return balance, if incorrect pin return "invalid pin"
+- deposit()
+if correct pin and given amnt, increment balance by the amount
+if pin is incorrect return "invalid pin"
+- withdraw()
+if pin is incorrect return "incorrect pin"
+if correct pin and w/d amnt is > bal, decrement balance balance -= amnt
+if amn > bal return "insufficient funds!"
+- changePin()
+if pin is correct update new pin to the pin number, if invalid pin return invalid
+*/
--- guessing-game.js
+++ guessing-game.js
@@ -1,26 +1,20 @@
 function guessingGame() {
-    const random = Math.floor(Math.random() * 100);
+    const randomNum = Math.floor(Math.random() * 100);
     let correctGuess = false;
+    let triedGuesses = 0;
     return function newRandoNum(num){
-        if(correctGuess) return "Game Over, you won!!";
-        if(random === num){
+        if(correctGuess) return "The game is over, you already won!"
+        triedGuesses++;
+        if(randomNum === num){
+            correctGuess = true;
+            return `You win! You found ${num} in ${triedGuesses} guesses.`;
 
         }
-
+        if(randomNum < num) return `${num} is too high!`;
+        if(randomNum > num) return `${num} is too low!`;
     }
 
 }
 
 module.exports = { guessingGame };
 
-/* Pseudo Code
-1- create a function called Guessing Game
-2- GuessingGame will return a function to guess a number between 0-99
-ex: Math.floor(Math.random() * 100)
-3- each time you call new game is called it should select a new random number *must be kept from the player*
-4- game should tell you if your guess is too high, too low or correct
---- ex: if(random < num) return `${num} is too high!`
-        if(random > num) return `${num} is correcr!`
-5- game ends if guess is correct
-
-/*
